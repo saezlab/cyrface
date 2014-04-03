2f11be960d1084d37ec967cf95310114923a1a0f
EmanuelGoncalves <emanuelvgoncalves@gmail.com>
2014-04-03 15:41:15 +0100

    Main package name changed to Cyrface2, to follow version number

--- src/main/java/uk/ac/ebi/cyrface3/internal/sbml/simplenet/NetworkLayout.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ebi.cyrface3.internal.sbml.simplenet;
-
-import java.awt.geom.Point2D;
-
-public interface NetworkLayout extends Network
-{
-	public abstract void getLocation (Node n, Point2D p);
-	public abstract void setLocation (Node n, Point2D p);
-}
